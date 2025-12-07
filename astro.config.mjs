116c8ff0484c49c378fc17fd67092e2f618dff5d
marmart1987 <marmtk1811@gmail.com>
2025-12-07 13:19:21 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://marmart1987.github.io',
+    base: '/blog',
+  })
